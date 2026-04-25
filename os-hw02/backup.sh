d7c70641aa797c3d97544a319354b52f90954359
Melted_Operator_Roma <ivanov07roman07@gmail.com>
2026-04-25 11:13:34 +0500

    добавил директорию backups куда буду резервно копироать другие директории

--- os-hw02/backup.sh
+++ os-hw02/backup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+#Использование: ./backup.sh /название директории
 set -euo pipefail
 
-if [[ $# -eq 0 ]]; then
-    echo "Ошибка: укажите файл"
-    exit 1
-fi
+#if [[ $# -eq 0 ]]; then
+#    echo "Ошибка: укажите файл"
+#    exit 1
+#fi
+#
+#if [[ -f "$1" ]]; then
+#    echo "Файл найден: $(wc -l < "$1") строк"
+#else
+#    echo "Файл '$1' не существует"
+#    exit 1
+#fi
+mkdir -p ~/os-systems-work/os-hw02/backups
 
-if [[ -f "$1" ]]; then
-    echo "Файл найден: $(wc -l < "$1") строк"
-else
-    echo "Файл '$1' не существует"
-    exit 1
-fi
