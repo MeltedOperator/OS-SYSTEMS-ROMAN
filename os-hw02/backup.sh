3369366ff97638088635f5119ad69bc936546733
Melted_Operator_Roma <ivanov07roman07@gmail.com>
2026-04-25 11:23:04 +0500

    вынес валидацию в отдельные функции и вывел их в main

--- os-hw02/backup.sh
+++ os-hw02/backup.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 #Использование: ./backup.sh /название директории
 set -euo pipefail
-
+validate_args() {
 if [[ $# -eq 0 ]]; then
     echo "Ошибка: укажите файл"
     exit 1
 fi
-
-if [[ -d "$1" ]]; then
-    echo "Директория найдена"
-else
-    echo "Директория '$1' существует"
+}
+validate_dir() {
+if [[ ! -d "$1" ]]; then
+    echo "Директория '$1' не существует"
     exit 1
 fi
-mkdir -p ~/os-systems-work/os-hw02/backups
+}
 
+main() {
+	local wanted_dir="$1"
+	validate_args "$1"
+	validate_dir "$1"
+
+
+}
+main "$@"
