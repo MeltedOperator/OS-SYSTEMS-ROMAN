40e34f9186d5557af2f8dfd82ea670dcfd217dab
Melted_Operator_Roma <ivanov07roman07@gmail.com>
2026-04-25 11:17:00 +0500

    добавил проверку на существование директории и был ли предоставлен аргумент. Если нет - выходит ошибка

--- os-hw02/backup.sh
+++ os-hw02/backup.sh
@@ -2,16 +2,16 @@
 #Использование: ./backup.sh /название директории
 set -euo pipefail
 
-#if [[ $# -eq 0 ]]; then
-#    echo "Ошибка: укажите файл"
-#    exit 1
-#fi
-#
-#if [[ -f "$1" ]]; then
-#    echo "Файл найден: $(wc -l < "$1") строк"
-#else
-#    echo "Файл '$1' не существует"
-#    exit 1
-#fi
+if [[ $# -eq 0 ]]; then
+    echo "Ошибка: укажите файл"
+    exit 1
+fi
+
+if [[ -d "$1" ]]; then
+    echo "Директория найдена"
+else
+    echo "Директория '$1' существует"
+    exit 1
+fi
 mkdir -p ~/os-systems-work/os-hw02/backups
 
