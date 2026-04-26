c047f73837259afa2703777aada35a6f9a45e292
Melted_Operator_Roma <ivanov07roman07@gmail.com>
2026-04-26 12:54:31 +0500

    backup.sh теперь может выполнять ротацию(удалять все кроме трех последних архивов) и добавлять новые архивы вместе с датой их создания

--- os-hw02/backup.sh
+++ os-hw02/backup.sh
@@ -2,24 +2,41 @@
 #Использование: ./backup.sh /название директории
 set -euo pipefail
 validate_args() {
-if [[ $# -eq 0 ]]; then
-    echo "Ошибка: укажите файл"
-    exit 1
-fi
+	if [[ $# -eq 0 ]]; then
+    		echo "Ошибка: укажите файл"
+    		exit 1
+	fi
 }
 validate_dir() {
-if [[ ! -d "$1" ]]; then
-    echo "Директория '$1' не существует"
-    exit 1
-fi
+	if [[ ! -d "$1" ]]; then
+    		echo "Директория '$1' не существует"
+    		exit 1
+	fi
+}
+
+create_backup() {
+	local copy_what="$1"
+	local where_to_copy="$2"
+        
+	tar -czf "$where_to_copy"/backup-$(date +%F_%H-%M-%S).tar.gz "$copy_what"
+}
+
+rotate_backups() {
+	backups="$1"
+	ls -t "$backups"/*.tar.gz 2>/dev/null | tail -n +3 | xargs -r rm -f
 }
 
 main() {
+	local wanted_dir="${1:-}"
+
+	validate_args "$wanted_dir"
+	validate_dir "$wanted_dir"
+
 	local reserve_dir="$HOME/os-systems-work/os-hw02/backups"
-	local wanted_dir="$1"
-	validate_args "$1"
-	validate_dir "$1"
-	tar -czf "$reserve_dir"/backup-$(date +%F).tar.gz "$1"
+	
+	create_backup "$wanted_dir" "$reserve_dir"
+	ls -la "$reserve_dir"
+	rotate_backups "$reserve_dir"
 
 
 }
