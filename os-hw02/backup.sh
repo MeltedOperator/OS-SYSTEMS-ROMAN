24e7842642278070460067f0ea99c06405b6d5ad
Melted_Operator_Roma <ivanov07roman07@gmail.com>
2026-04-26 16:46:56 +0500

    Теперь отчет выводится полностью, вместе с количеством файлов после ротации, с указанием того где будут лежать бэкапы и размером сохраненного архива

--- os-hw02/backup.sh
+++ os-hw02/backup.sh
@@ -28,16 +28,23 @@ create_backup() {
 
 rotate_backups() {
 	backups="$1"
-	ls -t "$backups"/*.tar.gz 2>/dev/null | tail -n +3 | xargs -r rm -f
+	ls -t "$backups"/*.tar.gz 2>/dev/null | tail -n +4 | xargs -r rm -f
 }
 
 print_report() {
-	local dir_to_report="$1" 
-	local archive="$2"
+	local dir_to_report="$wanted_dir" 
+	local archive="$archive"
+	local where_saved="$reserve_dir"
+
+	FILE_COUNT=$(ls -1 "$where_saved" | wc -l)
+
 	echo "=== РЕЗЕРВНОЕ КОПИРОВАНИЕ ==="
 	echo "Источник: $(dirname "$dir_to_report")"
 	echo "Архив: $(basename "$archive")"
-	echo "Размер: $(stat "$archive")"	
+	echo "Размер: $(stat -c%s "$archive")"
+	echo "Сохранено в "$where_saved" "
+	echo "Архивов после ротации: "$FILE_COUNT" "	
+	echo "=== Готово! ==="
 }
 
 main() {
@@ -50,11 +57,11 @@ main() {
 
 
 	archive=$(create_backup "$wanted_dir" "$reserve_dir")
-        #ls -la "$reserve_dir"
+        
 	
 	rotate_backups "$reserve_dir"
 	
-	print_report "$wanted_dir" "$archive"
-
+	print_report "$wanted_dir" "$archive" "$reserve_dir"
+	ls -la "$reserve_dir"
 }
 main "$@"
