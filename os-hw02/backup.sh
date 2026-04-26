22c5c69f32684836ae7bd5b80d4ade046d094546
Melted_Operator_Roma <ivanov07roman07@gmail.com>
2026-04-26 15:31:43 +0500

    Добавил первую версию вывода отчета и подправил логику create_backup() теперь tar не говорит что он убирает /. И все части директории вплоть до ее имени теперь размещены в отдельных переменных, которые можно будет при желании подкрутить

--- os-hw02/backup.sh
+++ os-hw02/backup.sh
@@ -3,13 +3,13 @@
 set -euo pipefail
 validate_args() {
 	if [[ $# -eq 0 ]]; then
-    		echo "Ошибка: укажите файл"
+    		echo "Резервное копирование не удалось. Не была передана директория"
     		exit 1
 	fi
 }
 validate_dir() {
 	if [[ ! -d "$1" ]]; then
-    		echo "Директория '$1' не существует"
+    		echo "Резервное копирование не удалось. Директория '$1' не существует"
     		exit 1
 	fi
 }
@@ -18,12 +18,26 @@ create_backup() {
 	local copy_what="$1"
 	local where_to_copy="$2"
         
-	tar -czf "$where_to_copy"/backup-$(date +%F_%H-%M-%S).tar.gz "$copy_what"
+	main_dir="$(dirname "$copy_what")"
+	directory_name="$(basename "$copy_what")"
+	archive="$where_to_copy/backup-$(date +%F_%H-%M-%S).tar.gz"
+
+	tar -czf "$archive" -C "$main_dir" "$directory_name"
+	echo "$archive"
 }
 
 rotate_backups() {
 	backups="$1"
 	ls -t "$backups"/*.tar.gz 2>/dev/null | tail -n +3 | xargs -r rm -f
+}
+
+print_report() {
+	local dir_to_report="$1" 
+	local archive="$2"
+	echo "=== РЕЗЕРВНОЕ КОПИРОВАНИЕ ==="
+	echo "Источник: $(dirname "$dir_to_report")"
+	echo "Архив: $(basename "$archive")"
+	echo "Размер: $(stat "$archive")"	
 }
 
 main() {
@@ -32,12 +46,15 @@ main() {
 	validate_args "$wanted_dir"
 	validate_dir "$wanted_dir"
 
-	local reserve_dir="$HOME/os-systems-work/os-hw02/backups"
-	
-	create_backup "$wanted_dir" "$reserve_dir"
-	ls -la "$reserve_dir"
-	rotate_backups "$reserve_dir"
+	reserve_dir="$HOME/os-systems-work/os-hw02/backups" 
 
+
+	archive=$(create_backup "$wanted_dir" "$reserve_dir")
+        #ls -la "$reserve_dir"
+	
+	rotate_backups "$reserve_dir"
+	
+	print_report "$wanted_dir" "$archive"
 
 }
 main "$@"
