5cc41ffe9bb9a6486ce5d4e9b04857dee5b9d209
Melted_Operator_Roma <ivanov07roman07@gmail.com>
2026-04-27 21:49:15 +0500

    Построил основную логику fs_audit.sh и добавил к основной проверке аргументов дополнительно проверку на то, чтобы переданная в качестве аргумента директория не была пуста

--- hw03/fs_audit.sh
+++ hw03/fs_audit.sh
@@ -1,15 +1,63 @@
 #!/bin/bash
-#использование: ./fs_audit.sh <имя директории>
-#TODO list: что делает fs_audit.sh 
-#берет директорию и выводит 4 секции:
-#TODO 1: Смонтированные ФС
-#TODO 2: Статистика Директории
-#TODO 3: Топ-5 Крупнейших файлов
-#TODO 4: df vs du
-#Критерии: 
-#bin/bash + set -euo pipefail
-#Функции show_mounted_f(), show_dir_stats()
-#show_top_files(), show_df_vs_du()
-#main()
-#Валидация $#, -d "$1"
-#shellcheck fs_audit.sh
+#использование: ./fs_audit.sh /имя директории
+set -euo pipefail
+
+validate_dir() {
+
+        if [[ $# -eq 0 ]]; then
+                echo "Резервное копирование не удалось. Не была передана директория"
+                exit 1
+        fi
+        #проверяем существование директории
+        if [[ ! -d "$1" ]]; then
+                echo "Неправильная директория! '$1'"
+                exit 1
+        fi
+
+	if [ ! -n "$( ls -A "$1" 2>/dev/null)" ]; then
+		echo "Введите не пустую директорию"
+		exit 1
+	fi
+}
+
+show_mounted_f() {
+	echo "=== ФС отчёт ==="
+	echo ""
+
+	echo "--- Смонтированные ФС ---"
+	df -Th 2>/dev/null | head -1
+	df -Th 2>/dev/null | grep -E 'ext4|tmpfs'
+	echo ""
+}
+
+show_dir_stats() {
+	local TARGET_DIR="$1"
+	echo "--- Статистика: $TARGET_DIR ---"
+	echo "  Файлов:     $(find "$TARGET_DIR" -type f 2>/dev/null | wc -l)"
+	echo "  Директорий: $(find "$TARGET_DIR" -type d 2>/dev/null | wc -l)"
+	echo "  Симлинков:   $(find "$TARGET_DIR" -type l 2>/dev/null | wc -l)"
+	echo ""
+
+}
+
+show_top_files() {
+	local TARGET_DIR="$1"
+	echo "--- Топ-3 крупнейших файла ---"
+	printf "  %-10s %-12s %s\n" "Inode" "Размер" "Путь"
+	find "$TARGET_DIR" -type f -printf '%i\t%s\t%p\n' 2>/dev/null \
+   	  | sort -t$'\t' -k2 -rn \
+   	  | head -3 \
+   	  | while IFS=$'\t' read -r inode size path; do
+       		 hr_size=$(numfmt --to=iec-i "$size" 2>/dev/null || echo "${size}B")
+       		 printf "  %-10s %-12s %s\n" "$inode" "$hr_size" "$path"
+   	  done
+}
+
+main() {
+        local wanted_dir="${1:-}"
+	validate_dir "$wanted_dir"
+	show_mounted_f 
+	show_dir_stats "$wanted_dir"
+	show_top_files "$wanted_dir"
+}
+main "$@"
