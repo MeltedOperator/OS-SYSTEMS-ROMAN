99396b3acd2da35f00116cc34aa2c97fd2302630
Melted_Operator_Roma <ivanov07roman07@gmail.com>
2026-05-10 00:44:33 +0500

    Выгружаю домашнее задание 6

--- hw06/scheduler_experiment.sh
+++ hw06/scheduler_experiment.sh
@@ -6,8 +6,24 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
-sudo nice -n -10  dd if=/dev/urandom of=/dev/null bs=1M count=200 2>/dev/null &
-PID_HIGH=$!
-sudo nice -n 19 dd if=/dev/urandom of=/dev/null bs=1M count=200 2>/dev/null &
-PID_LOW=$!
-wait
+for _ in $(seq 1 3); do
+    dd if=/dev/urandom of=/dev/null bs=1M &
+done
+
+# Шаг 2: Два замера одновременно
+default_out="~/os-systems-work/hw06/README.md"
+
+out_file="${1:-$default_out}"
+
+{ time nice -n 19  dd if=/dev/urandom of=/dev/null bs=1M count=1 2>/dev/null ; } 2>> "$out_file" &
+pid1=$!
+{ time nice -n -10 dd if=/dev/urandom of=/dev/null bs=1M count=1 2>/dev/null ; } 2>> "$out_file" &
+pid2=$!
+wait $pid1 $pid2
+
+# Шаг 3: Сравнить
+echo "=== nice 19 === " && cat "$out_file"
+echo "=== nice -10 === " && cat "$out_file"
+
+# Шаг 4: Убить фоновые
+kill %1 %2 %3 
